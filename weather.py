4485450ee7e2ab7ea1e51ccea3b58ef0b08f9ebd
AaronSledge <ahern2118@gmail.com>
2024-07-28 03:08:08 -0700

    Fetched data from api

--- weather.py
+++ weather.py
@@ -1,5 +1,11 @@
 import requests
-from bs4 import BeautifulSoup
+import cred
 from winotify import Notification
 
-print("hi")
+session = requests.session()
+
+url = f"https://api.openweathermap.org/data/2.5/weather?q=Riverside,us&appid={cred.api_key}&units=imperial"
+response = session.get(url)
+
+
+print(response.json())
